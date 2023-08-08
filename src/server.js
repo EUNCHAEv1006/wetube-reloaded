2f39991a718c040c05a395a1e4d3d38b845f87d3
EUNCHAEv1006 <ke808762@gmail.com>
2023-08-08 22:28:33 +0900

    FEAT: GET Requests, Responses

--- src/server.js
+++ src/server.js
@@ -2,7 +2,18 @@ import express from "express";
 
 const PORT = 4000;
 
-const app = express();
+const app = express(); /* create application */
+
+const handleHome = (req, res) /* (request, response) 형식 */ => {
+    return res.send("<h1>I still love you.<h1>");
+}
+
+const handleLogin = (req, res) => {
+    return res.send("Login here.");
+}
+
+app.get("/", handleHome); /* button.addEventListener("click", handleClick)과 동일함 */
+app.get("/login", handleLogin);
 
 const handleListening = () => 
     console.log(`✅ Server listening on port http://localhost:${PORT}🚀`);
