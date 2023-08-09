04d1dc8e1762ef63b2bfb7fbdf294e3768ce8b82
EUNCHAEv1006 <ke808762@gmail.com>
2023-08-09 17:14:03 +0900

    🆕 NEW: Implemented new controllers for exports

--- src/server.js
+++ src/server.js
@@ -16,6 +16,6 @@ app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
 const handleListening = () => 
-    console.log(`✅ Server listening on port http://localhost:${PORT}🚀`);
+    console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
 
 app.listen(PORT, handleListening);
