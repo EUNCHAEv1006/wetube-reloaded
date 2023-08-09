91e84d2f785a68f6b2a2b4898f9856603de88764
EUNCHAEv1006 <ke808762@gmail.com>
2023-08-09 22:01:58 +0900

    ✨ FEAT: Returning HTML

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -4,8 +4,8 @@ import { see, edit, upload, deleteVideo } from "../controllers/videoController";
 const videoRouter = express.Router();
 
 videoRouter.get("/:id(\\d+)", see); /* express가 숫자만 받아옴 */
-videoRouter.get("/:id(\\d+)", edit);
-videoRouter.get("/:id(\\d+)", deleteVideo);
+videoRouter.get("/:id(\\d+)/edit", edit);
+videoRouter.get("/:id(\\d+)/delete", deleteVideo);
 videoRouter.get("/upload", upload);
 
 export default videoRouter;
