289d52c2b8e0a81a6f3f62939e3b1966b48fa656
EUNCHAEv1006 <ke808762@gmail.com>
2023-08-11 00:44:06 +0900

    💄  DESIGN: Array Database

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { see, edit, upload, deleteVideo } from "../controllers/videoController";
+import { watch, edit, upload, deleteVideo } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/:id(\\d+)", see); /* express가 숫자만 받아옴 */
+videoRouter.get("/:id(\\d+)", watch); /* express가 숫자만 받아옴 */
 videoRouter.get("/:id(\\d+)/edit", edit);
 videoRouter.get("/:id(\\d+)/delete", deleteVideo);
 videoRouter.get("/upload", upload);
