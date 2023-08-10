c5fc96b965512b08ba6ae6616fca480bea6e0613
EUNCHAEv1006 <ke808762@gmail.com>
2023-08-11 01:14:57 +0900

    ✨ FEAT: Edit Video

--- src/routers/globerRouter.js
+++ src/routers/globerRouter.js
@@ -7,6 +7,5 @@ const globalRouter = express.Router();
 globalRouter.get("/", trending); /* home */
 globalRouter.get("/join", join);
 globalRouter.get("/login", login);
-globalRouter.get("/search", search);
 
 export default globalRouter;
