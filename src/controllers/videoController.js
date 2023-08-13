67b1cd8aa7ba1b4f485bf2b662ee3e11cfed8ad3
EUNCHAEv1006 <ke808762@gmail.com>
2023-08-13 22:28:44 +0900

    🚧 FEAT: Query

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,8 +1,12 @@
 import Video from "../models/Video";
 
 export const home = (req, res) => {
-  Video.find({}, (error, videos) => {});
-  return res.render("home", { pageTitle: "Home" });
+  console.log("Start");
+  Video.find({}, (error, videos) => {
+    console.log("Finished");
+    return res.render("home", { pageTitle: "Home", videos });
+  });
+  console.log("I finish first");
 };
 export const watch = (req, res) => {
   const { id } = req.params;
@@ -24,6 +28,5 @@ export const getUpload = (req, res) => {
 
 export const postUpload = (req, res) => {
   const { title } = req.body;
-
   return res.redirect("/");
 };
