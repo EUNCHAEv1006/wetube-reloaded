a5dd77b8b05579d75069234a9e09129b9e31abbc
EUNCHAEv1006 <ke808762@gmail.com>
2023-08-14 02:27:44 +0900

    🎨 COMMENT: Returns and Renders

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,21 +1,7 @@
 import Video from "../models/Video";
 
-/* callback 방법
-
-Video.find({}, (error, videos) => {
-  if(error){
-    return res.render("server-error");
-  }
-  return res.render("hone", { pageTitle: "Home", videos });
-}); 
-
-*/
-
 export const home = async (req, res) => {
-  /* 해당 함수가 asynchronous 일 때만 사용가능해서 async 사용해줌 */
-  const videos = await Video.find(
-    {}
-  ); /* await는 database를 기다려줌 , 함수 안에서만 사용 가능 */
+  const videos = await Video.find({});
   return res.render("home", { pageTitle: "Home", videos });
 };
 export const watch = (req, res) => {
