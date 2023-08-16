e4b6873e6f559e5b9b3ef8353bf97e05159fae9e
EUNCHAEv1006 <ke808762@gmail.com>
2023-08-16 22:15:35 +0900

    🚧 FEAT: Create Account Part

--- src/server.js
+++ src/server.js
@@ -1,18 +1,17 @@
 import express from "express";
 import morgan from "morgan";
-import globalRouter from "./routers/globerRouter";
+import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
-import "dotenv/config";
 
 const app = express();
 const logger = morgan("dev");
 
-app.set("view engine", "pug"); /* pug를 view engine으로 설정 */
+app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
-app.use("/", globalRouter);
+app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
