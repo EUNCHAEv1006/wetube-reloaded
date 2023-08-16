5af54f6485a5f2daaa43c187794ffcbb1eff7446
EUNCHAEv1006 <ke808762@gmail.com>
2023-08-16 21:57:07 +0900

    ✨ FEAT: Search

--- src/routers/globerRouter.js
+++ src/routers/globerRouter.js
@@ -4,8 +4,9 @@ import { home, search } from "../controllers/videoController";
 
 const globalRouter = express.Router();
 
-globalRouter.get("/", home); /* home */
+globalRouter.get("/", home);
 globalRouter.get("/join", join);
 globalRouter.get("/login", login);
+globalRouter.get("/search", search);
 
 export default globalRouter;
