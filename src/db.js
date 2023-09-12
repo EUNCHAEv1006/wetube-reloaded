d1b5699a106f1ff33b110f6c9f5f37672b417129
EUNCHAEv1006 <ke808762@gmail.com>
2023-09-12 16:27:20 +0900

    Environment Variables

--- src/db.js
+++ src/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-mongoose.connect(procee.env.DB_URL, {
+mongoose.connect(process.env.DB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false,
