a8648a021437b35c5b0ea96e8f44f99013799af8
김은채 <ke808762@gmail.com>
2023-09-26 15:56:21 +0900

    🚧 FEAT: Comment Ids

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -12,6 +12,7 @@ export const home = async (req, res) => {
 export const watch = async (req, res) => {
   const { id } = req.params;
   const video = await Video.findById(id).populate("owner").populate("comments");
+  console.log(video);
   if (!video) {
     return res.render("404", { pageTitle: "Video not found." });
   }
@@ -146,5 +147,5 @@ export const createComment = async (req, res) => {
   });
   video.comments.push(comment._id);
   video.save();
-  return res.sendStatus(201);
+  return res.status(201).json({ newCommentId: comment._id });
 };
