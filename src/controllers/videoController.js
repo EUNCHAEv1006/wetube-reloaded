85fd22ca4aca16887576f06882076b90f299abea
김은채 <ke808762@gmail.com>
2023-09-26 10:43:01 +0900

    🚧 FEAT: Route part Two

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -27,7 +27,7 @@ export const getEdit = async (req, res) => {
     return res.status(404).render("404", { pageTitle: "Video not found." });
   }
   if (String(video.owner) !== String(_id)) {
-    req.flash("error", "Notauthorized");
+    req.flash("error", "Not authorized");
     return res.status(403).redirect("/");
   }
   return res.render("edit", { pageTitle: `Edit: ${video.title}`, video });
@@ -44,7 +44,7 @@ export const postEdit = async (req, res) => {
     return res.status(404).render("404", { pageTitle: "Video not found." });
   }
   if (String(video.owner) !== String(_id)) {
-    req.flash("error", "You are not the owner of the video.");
+    req.flash("error", "You are not the the owner of the video.");
     return res.status(403).redirect("/");
   }
   await Video.findByIdAndUpdate(id, {
@@ -71,7 +71,7 @@ export const postUpload = async (req, res) => {
       title,
       description,
       fileUrl: video[0].path,
-      thumbUrl: thumb[0].path.replace(/[\\]/g, "/"),
+      thumbUrl: thumb[0].path,
       owner: _id,
       hashtags: Video.formatHashtags(hashtags),
     });
@@ -130,6 +130,7 @@ export const registerView = async (req, res) => {
 
 export const createComment = (req, res) => {
   console.log(req.params);
-  console.req.body;
+  console.log(req.body);
+  console.log(req.body.text);
   return res.end();
 };
